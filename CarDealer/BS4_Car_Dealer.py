7b89d48be4ef5175af4a1b62ffbddb4d854ca045
Muhannad mansour <102443619+Muhannad0101@users.noreply.github.com>
2023-06-16 19:42:15 +0300

    Add files via upload

---
+++ CarDealer/BS4_Car_Dealer.py
@@ -0,0 +1,160 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd 
+
+website = 'https://www.cars.com/shopping/results/?stock_type=cpo&makes%5B%5D=mercedes_benz&models%5B%5D=&list_price_max=&maximum_distance=20&zip='
+
+response = requests.get(website)
+
+response.status_code
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+results = soup.find_all('div', {'class' : 'vehicle-card'})
+
+len(results)
+
+# Name
+results[0].find('h2').get_text()
+
+# Mileage
+results[0].find('div', {'class':'mileage'}).get_text()
+
+# Dealer Name
+results[0].find('div', {'class':'dealer-name'}).get_text().strip()
+
+# Rating
+results[0].find('span', {'class':'sds-rating__count'}).get_text()
+
+# Review Count
+results[0].find('span', {'class':'sds-rating__link'}).get_text()
+
+# Price
+results[0].find('span', {'class':'primary-price'}).get_text()
+
+# Put everything together inside a For-Loop
+name = []
+mileage = []
+dealer_name = []
+rating = []
+review_count = []
+price = []
+
+for result in results:
+    
+    # name
+    try:
+        name.append(result.find('h2').get_text()) 
+    except:
+        name.append('n/a')
+    
+    # mileage
+    try:
+        mileage.append(result.find('div', {'class':'mileage'}).get_text())
+    except:
+        mileage.append('n/a')
+    
+    # dealer_name
+    try:
+        dealer_name.append(result.find('div', {'class':'dealer-name'}).get_text().strip())
+    except:
+        dealer_name.append('n/a')
+        
+    # rating
+    try:
+        rating.append(result.find('span', {'class':'sds-rating__count'}).get_text())
+    except:
+        rating.append('n/a')
+    
+    # review_count
+    try:
+        review_count.append(result.find('span', {'class':'sds-rating__link'}).get_text())
+    except:
+        review_count.append('n/a')
+    
+    #price 
+    try:
+        price.append(result.find('span', {'class':'primary-price'}).get_text())
+    except:
+        price.append('n/a')
+
+
+# Create Pandas Dataframe
+car_dealer = pd.DataFrame({'Name': name, 'Mileage':mileage, 'Dealer Name':dealer_name,
+                                'Rating': rating, 'Review Count': review_count, 'Price': price})
+
+
+# Data Cleaning
+car_dealer['Review Count'] = car_dealer['Review Count'].apply(lambda x: x.strip('reviews)').strip('('))
+
+# Output in Excel
+# car_dealer.to_excel('car_dealer_single_page.xlsx', index=False)
+
+
+
+# Part 2 - Pagination
+name = []
+mileage = []
+dealer_name = []
+rating = []
+review_count = []
+price = []
+
+for i in range (1,11):
+    
+    # website in variable
+    website = 'https://www.cars.com/shopping/results/?page='+ str(i) +'&page_size=20&dealer_id=&list_price_max=&list_price_min=&makes[]=mercedes_benz&maximum_distance=20&mileage_max=&sort=best_match_desc&stock_type=cpo&year_max=&year_min=&zip=' 
+    
+    # request to website
+    response = requests.get(website)
+    
+    # soup object
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # results
+    results = soup.find_all('div', {'class' : 'vehicle-card'})
+    
+    # loop through results
+    for result in results:
+    
+        # name
+        try:
+            name.append(result.find('h2').get_text()) 
+        except:
+            name.append('n/a')
+
+        # mileage
+        try:
+            mileage.append(result.find('div', {'class':'mileage'}).get_text())
+        except:
+            mileage.append('n/a')
+
+        # dealer_name
+        try:
+            dealer_name.append(result.find('div', {'class':'dealer-name'}).get_text().strip())
+        except:
+            dealer_name.append('n/a')
+
+        # rating
+        try:
+            rating.append(result.find('span', {'class':'sds-rating__count'}).get_text())
+        except:
+            rating.append('n/a')
+
+        # review_count
+        try:
+            review_count.append(result.find('span', {'class':'sds-rating__link'}).get_text())
+        except:
+            review_count.append('n/a')
+
+        #price 
+        try:
+            price.append(result.find('span', {'class':'primary-price'}).get_text())
+        except:
+            price.append('n/a')
+# dictionary
+car_dealer = pd.DataFrame({'Name': name, 'Mileage':mileage, 'Dealer Name':dealer_name,
+                                'Rating': rating, 'Review Count': review_count, 'Price': price})
+
+car_dealer['Review Count'] = car_dealer['Review Count'].apply(lambda x: x.strip('reviews)').strip('('))
+car_dealer.to_csv('car_dealer_data.csv')
